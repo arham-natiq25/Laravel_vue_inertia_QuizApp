b43d839a1ec5ca272dd62b8b2cd6fc8584eea23a
arham-natiq25 <arhamnatiq25@gmail.com>
2024-11-02 03:49:04 +0500

    Some Style changes added in quiz App

--- resources/js/Shared/Header.vue
+++ resources/js/Shared/Header.vue
@@ -6,17 +6,17 @@ const page = usePage()
 const appName = computed(()=>page.props.appName)
 </script>
 <template>
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom bg-success">
         <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
           <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-          <span class="fs-4">{{ appName }}</span>
+          <span class="fs-4 text-white">{{ appName }}</span>
         </a>
 
         <ul class="nav nav-pills">
-          <li class="nav-item"><Link href="/" :class="{'active':$page.url==='/'}" class="nav-link" aria-current="page">Home</Link></li>
-          <li class="nav-item"><Link href="/quiz" :class="{'active':$page.url==='/quiz'}" class="nav-link">Quiz</Link></li>
-          <li class="nav-item"><Link href="/questions"  :class="{'active':$page.url==='/questions'}" class="nav-link">Questions</Link></li>
-          <li class="nav-item"><Link href="/results"  :class="{'active':$page.url==='/results'}" class="nav-link">Results</Link></li>
+          <li class="nav-item"><Link href="/" :class="{'active':$page.url==='/'}" class="nav-link text-white" aria-current="page">Home</Link></li>
+          <li class="nav-item"><Link href="/quiz" :class="{'active':$page.url==='/quiz'}" class="nav-link text-white">Quiz</Link></li>
+          <li class="nav-item"><Link href="/questions"  :class="{'active':$page.url==='/questions'}" class="nav-link text-white">Questions</Link></li>
+          <li class="nav-item"><Link href="/results"  :class="{'active':$page.url==='/results'}" class="nav-link text-white">Results</Link></li>
         </ul>
       </header>
 </template>
